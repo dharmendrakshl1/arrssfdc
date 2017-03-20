bc17e2c35bb3d6e749d0064c821f6cbc6fa0793a
dharmendra.kshl1@gmail.com <dharmendra.kshl1@gmail.com>
2017-03-20 17:33:49 +0530

    Changing code according to handle the Error in case of either Salesforce is down or Invalid Credential or Malformed Query and etc.

--- sfdcsmimupsertproducts/src/com/arris/sfdc/api/rest/ServiceMaxProductsOperationService.java
+++ sfdcsmimupsertproducts/src/com/arris/sfdc/api/rest/ServiceMaxProductsOperationService.java
@@ -12,6 +12,19 @@ import org.apache.log4j.Logger;
 import com.arris.sfdc.pojo.UpsertProductInput;
 import com.arris.sfdc.pojo.UpsertProductOutput;
 import com.arris.sfdc.service.provider.ServiceMaxProductsOperationServiceProvider;
+import com.sforce.soap.enterprise.fault.ApiFault;
+import com.sforce.soap.enterprise.fault.ApiQueryFault;
+import com.sforce.soap.enterprise.fault.InvalidFieldFault;
+import com.sforce.soap.enterprise.fault.InvalidIdFault;
+import com.sforce.soap.enterprise.fault.InvalidNewPasswordFault;
+import com.sforce.soap.enterprise.fault.InvalidQueryLocatorFault;
+import com.sforce.soap.enterprise.fault.InvalidSObjectFault;
+import com.sforce.soap.enterprise.fault.LoginFault;
+import com.sforce.soap.enterprise.fault.MalformedQueryFault;
+import com.sforce.soap.enterprise.fault.MalformedSearchFault;
+import com.sforce.soap.enterprise.fault.UnexpectedErrorFault;
+import com.sforce.ws.ConnectionException;
+import com.sforce.ws.SoapFaultException;
 
 @Path("/operations")
 public class ServiceMaxProductsOperationService {
@@ -28,7 +41,67 @@ public class ServiceMaxProductsOperationService {
 		UpsertProductOutput upsertProductOutput = null;
 		
 		if(upsertProductInput != null){
-			upsertProductOutput = serviceMaxProductsOperationServiceProvider.performProdcutOperation(upsertProductInput);
+			try{
+				upsertProductOutput = serviceMaxProductsOperationServiceProvider.performProdcutOperation(upsertProductInput);
+				logger.info("upsertProductOutput : "+upsertProductOutput);
+				
+			}catch(LoginFault e){
+				logger.error("LoginFault Error : ERROR_CODE : "+e.getExceptionCode()+", ERROR_MESSAGE : "+e.getExceptionMessage());
+				return Response.status(401).header("statusMessage", "ERROR_CODE : "+e.getExceptionCode()+", ERROR_MESSAGE : "+e.getExceptionMessage()).entity(upsertProductOutput).build();
+				
+			}catch(InvalidFieldFault e){
+				logger.error("InvalidFieldFault Error : ERROR_CODE : "+e.getExceptionCode()+", ERROR_MESSAGE : "+e.getExceptionMessage());
+				return Response.status(500).header("statusMessage", "ERROR_CODE : "+e.getExceptionCode()+", ERROR_MESSAGE : "+e.getExceptionMessage()).entity(upsertProductOutput).build();
+				
+			}catch(InvalidIdFault e){
+				logger.error("InvalidIdFault Error : ERROR_CODE : "+e.getExceptionCode()+", ERROR_MESSAGE : "+e.getExceptionMessage());
+				return Response.status(500).header("statusMessage", "ERROR_CODE : "+e.getExceptionCode()+", ERROR_MESSAGE : "+e.getExceptionMessage()).entity(upsertProductOutput).build();
+				
+			}catch(InvalidQueryLocatorFault e){
+				logger.error("InvalidQueryLocatorFault Error : ERROR_CODE : "+e.getExceptionCode()+", ERROR_MESSAGE : "+e.getExceptionMessage());
+				return Response.status(500).header("statusMessage", "ERROR_CODE : "+e.getExceptionCode()+", ERROR_MESSAGE : "+e.getExceptionMessage()).entity(upsertProductOutput).build();
+				
+			}catch(InvalidSObjectFault e){
+				logger.error("InvalidSObjectFault Error : ERROR_CODE : "+e.getExceptionCode()+", ERROR_MESSAGE : "+e.getExceptionMessage());
+				return Response.status(500).header("statusMessage", "ERROR_CODE : "+e.getExceptionCode()+", ERROR_MESSAGE : "+e.getExceptionMessage()).entity(upsertProductOutput).build();
+				
+			}catch(MalformedQueryFault e){
+				logger.error("MalformedQueryFault Error : ERROR_CODE : "+e.getExceptionCode()+", ERROR_MESSAGE : "+e.getExceptionMessage());
+				return Response.status(500).header("statusMessage", "ERROR_CODE : "+e.getExceptionCode()+", ERROR_MESSAGE : "+e.getExceptionMessage()).entity(upsertProductOutput).build();
+				
+			}catch(MalformedSearchFault e){
+				logger.error("MalformedSearchFault Error : ERROR_CODE : "+e.getExceptionCode()+", ERROR_MESSAGE : "+e.getExceptionMessage());
+				return Response.status(500).header("statusMessage", "ERROR_CODE : "+e.getExceptionCode()+", ERROR_MESSAGE : "+e.getExceptionMessage()).entity(upsertProductOutput).build();
+				
+			}catch(UnexpectedErrorFault e){
+				logger.error("UnexpectedErrorFault Error : ERROR_CODE : "+e.getExceptionCode()+", ERROR_MESSAGE : "+e.getExceptionMessage());
+				return Response.status(500).header("statusMessage", "ERROR_CODE : "+e.getExceptionCode()+", ERROR_MESSAGE : "+e.getExceptionMessage()).entity(upsertProductOutput).build();
+				
+			}catch(InvalidNewPasswordFault e){
+				logger.error("InvalidNewPasswordFault Error : ERROR_CODE : "+e.getExceptionCode()+", ERROR_MESSAGE : "+e.getExceptionMessage());
+				return Response.status(500).header("statusMessage", "ERROR_CODE : "+e.getExceptionCode()+", ERROR_MESSAGE : "+e.getExceptionMessage()).entity(upsertProductOutput).build();
+				
+			}catch(ApiQueryFault e){
+				logger.error("ApiQueryFault Error : ERROR_CODE : "+e.getExceptionCode()+", ERROR_MESSAGE : "+e.getExceptionMessage());
+				return Response.status(500).header("statusMessage", "ERROR_CODE : "+e.getExceptionCode()+", ERROR_MESSAGE : "+e.getExceptionMessage()).entity(upsertProductOutput).build();
+				
+			}catch(ApiFault e){
+				logger.error("ApiFault Error : ERROR_CODE : "+e.getExceptionCode()+", ERROR_MESSAGE : "+e.getExceptionMessage());
+				return Response.status(500).header("statusMessage", "ERROR_CODE : "+e.getExceptionCode()+", ERROR_MESSAGE : "+e.getExceptionMessage()).entity(upsertProductOutput).build();
+				
+			}catch(SoapFaultException e){
+				logger.error("SoapFaultException Exception : ERROR_CODE : "+e.getFaultCode()+", ERROR_MESSAGE : "+e.getMessage());
+				return Response.status(500).header("statusMessage", "ERROR_CODE : "+e.getFaultCode()+", ERROR_MESSAGE : "+e.getMessage()).entity(upsertProductOutput).build();
+				
+			}catch(ConnectionException e){
+				logger.error("ConnectionException Exception : ERROR_MESSAGE : "+e.getMessage());
+				return Response.status(500).header("statusMessage", "ERROR_MESSAGE : "+e.getMessage()).entity(upsertProductOutput).build();
+				
+			}catch(Exception e) {
+				logger.error("Exception : ERROR_MESSAGE : "+e.getMessage());
+				return Response.status(500).header("statusMessage", "ERROR_MESSAGE : "+e.getMessage()).entity(upsertProductOutput).build();
+				
+			}
 		}
 		
 		logger.info("Leaving - com.arris.sfdc.api.rest.ServiceMaxProductsOperationService.performProdcutOperation(UpsertProductInput) - upsertProductOutput : "+upsertProductOutput);
