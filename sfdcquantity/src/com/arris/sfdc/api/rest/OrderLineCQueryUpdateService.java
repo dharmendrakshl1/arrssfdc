d3a6cd1315209aa22e47ec5d6c7a1f4e089f6069
dharmendra.kshl1@gmail.com <dharmendra.kshl1@gmail.com>
2017-03-20 15:16:07 +0530

    Changing code according to handle the Error in case of either Salesforce is down or Invalid Credential or Malformed Query and etc.

--- sfdcquantity/src/com/arris/sfdc/api/rest/OrderLineCQueryUpdateService.java
+++ sfdcquantity/src/com/arris/sfdc/api/rest/OrderLineCQueryUpdateService.java
@@ -12,6 +12,19 @@ import org.apache.log4j.Logger;
 import com.arris.sfdc.pojo.LineDetails;
 import com.arris.sfdc.pojo.UpdateOutput;
 import com.arris.sfdc.service.provider.OrderLineCQueryUpdateServiceProvider;
+import com.sforce.soap.enterprise.fault.ApiFault;
+import com.sforce.soap.enterprise.fault.ApiQueryFault;
+import com.sforce.soap.enterprise.fault.InvalidFieldFault;
+import com.sforce.soap.enterprise.fault.InvalidIdFault;
+import com.sforce.soap.enterprise.fault.InvalidNewPasswordFault;
+import com.sforce.soap.enterprise.fault.InvalidQueryLocatorFault;
+import com.sforce.soap.enterprise.fault.InvalidSObjectFault;
+import com.sforce.soap.enterprise.fault.LoginFault;
+import com.sforce.soap.enterprise.fault.MalformedQueryFault;
+import com.sforce.soap.enterprise.fault.MalformedSearchFault;
+import com.sforce.soap.enterprise.fault.UnexpectedErrorFault;
+import com.sforce.ws.ConnectionException;
+import com.sforce.ws.SoapFaultException;
 
 @Path("/orderlinecqueryupdate")
 public class OrderLineCQueryUpdateService {
@@ -27,8 +40,67 @@ public class OrderLineCQueryUpdateService {
 		OrderLineCQueryUpdateServiceProvider cQueryUpdateServiceProvider = new OrderLineCQueryUpdateServiceProvider();
 		UpdateOutput updateOutput = null;
 		
-		updateOutput = cQueryUpdateServiceProvider.orderLineCQureyOrUpdate(lineDetails);
-		
+		if(lineDetails != null){
+			try{
+				updateOutput = cQueryUpdateServiceProvider.orderLineCQureyOrUpdate(lineDetails);
+			}catch(LoginFault e){
+				logger.error("LoginFault Error : ERROR_CODE : "+e.getExceptionCode()+", ERROR_MESSAGE : "+e.getExceptionMessage());
+				return Response.status(401).header("statusMessage", "ERROR_CODE : "+e.getExceptionCode()+", ERROR_MESSAGE : "+e.getExceptionMessage()).entity(updateOutput).build();
+				
+			}catch(InvalidFieldFault e){
+				logger.error("InvalidFieldFault Error : ERROR_CODE : "+e.getExceptionCode()+", ERROR_MESSAGE : "+e.getExceptionMessage());
+				return Response.status(500).header("statusMessage", "ERROR_CODE : "+e.getExceptionCode()+", ERROR_MESSAGE : "+e.getExceptionMessage()).entity(updateOutput).build();
+				
+			}catch(InvalidIdFault e){
+				logger.error("InvalidIdFault Error : ERROR_CODE : "+e.getExceptionCode()+", ERROR_MESSAGE : "+e.getExceptionMessage());
+				return Response.status(500).header("statusMessage", "ERROR_CODE : "+e.getExceptionCode()+", ERROR_MESSAGE : "+e.getExceptionMessage()).entity(updateOutput).build();
+				
+			}catch(InvalidQueryLocatorFault e){
+				logger.error("InvalidQueryLocatorFault Error : ERROR_CODE : "+e.getExceptionCode()+", ERROR_MESSAGE : "+e.getExceptionMessage());
+				return Response.status(500).header("statusMessage", "ERROR_CODE : "+e.getExceptionCode()+", ERROR_MESSAGE : "+e.getExceptionMessage()).entity(updateOutput).build();
+				
+			}catch(InvalidSObjectFault e){
+				logger.error("InvalidSObjectFault Error : ERROR_CODE : "+e.getExceptionCode()+", ERROR_MESSAGE : "+e.getExceptionMessage());
+				return Response.status(500).header("statusMessage", "ERROR_CODE : "+e.getExceptionCode()+", ERROR_MESSAGE : "+e.getExceptionMessage()).entity(updateOutput).build();
+				
+			}catch(MalformedQueryFault e){
+				logger.error("MalformedQueryFault Error : ERROR_CODE : "+e.getExceptionCode()+", ERROR_MESSAGE : "+e.getExceptionMessage());
+				return Response.status(500).header("statusMessage", "ERROR_CODE : "+e.getExceptionCode()+", ERROR_MESSAGE : "+e.getExceptionMessage()).entity(updateOutput).build();
+				
+			}catch(MalformedSearchFault e){
+				logger.error("MalformedSearchFault Error : ERROR_CODE : "+e.getExceptionCode()+", ERROR_MESSAGE : "+e.getExceptionMessage());
+				return Response.status(500).header("statusMessage", "ERROR_CODE : "+e.getExceptionCode()+", ERROR_MESSAGE : "+e.getExceptionMessage()).entity(updateOutput).build();
+				
+			}catch(UnexpectedErrorFault e){
+				logger.error("UnexpectedErrorFault Error : ERROR_CODE : "+e.getExceptionCode()+", ERROR_MESSAGE : "+e.getExceptionMessage());
+				return Response.status(500).header("statusMessage", "ERROR_CODE : "+e.getExceptionCode()+", ERROR_MESSAGE : "+e.getExceptionMessage()).entity(updateOutput).build();
+				
+			}catch(InvalidNewPasswordFault e){
+				logger.error("InvalidNewPasswordFault Error : ERROR_CODE : "+e.getExceptionCode()+", ERROR_MESSAGE : "+e.getExceptionMessage());
+				return Response.status(500).header("statusMessage", "ERROR_CODE : "+e.getExceptionCode()+", ERROR_MESSAGE : "+e.getExceptionMessage()).entity(updateOutput).build();
+				
+			}catch(ApiQueryFault e){
+				logger.error("ApiQueryFault Error : ERROR_CODE : "+e.getExceptionCode()+", ERROR_MESSAGE : "+e.getExceptionMessage());
+				return Response.status(500).header("statusMessage", "ERROR_CODE : "+e.getExceptionCode()+", ERROR_MESSAGE : "+e.getExceptionMessage()).entity(updateOutput).build();
+				
+			}catch(ApiFault e){
+				logger.error("ApiFault Error : ERROR_CODE : "+e.getExceptionCode()+", ERROR_MESSAGE : "+e.getExceptionMessage());
+				return Response.status(500).header("statusMessage", "ERROR_CODE : "+e.getExceptionCode()+", ERROR_MESSAGE : "+e.getExceptionMessage()).entity(updateOutput).build();
+				
+			}catch(SoapFaultException e){
+				logger.error("SoapFaultException Exception : ERROR_CODE : "+e.getFaultCode()+", ERROR_MESSAGE : "+e.getMessage());
+				return Response.status(500).header("statusMessage", "ERROR_CODE : "+e.getFaultCode()+", ERROR_MESSAGE : "+e.getMessage()).entity(updateOutput).build();
+				
+			}catch(ConnectionException e){
+				logger.error("ConnectionException Exception : ERROR_MESSAGE : "+e.getMessage());
+				return Response.status(500).header("statusMessage", "ERROR_MESSAGE : "+e.getMessage()).entity(updateOutput).build();
+				
+			}catch(Exception e) {
+				logger.error("Exception : ERROR_MESSAGE : "+e.getMessage());
+				return Response.status(500).header("statusMessage", "ERROR_MESSAGE : "+e.getMessage()).entity(updateOutput).build();
+				
+			}
+		}
 		logger.info("Leaving - com.arris.sfdc.api.rest.OrderLineCQueryUpdateService.orderLineCQureyOrUpdate(LineDetails) - updateOutput : "+updateOutput);
 		return Response.status(200).entity(updateOutput).build();
 	}
--- sfdcquantity/src/com/arris/sfdc/service/provider/OrderLineCQueryUpdateServiceProvider.java
+++ sfdcquantity/src/com/arris/sfdc/service/provider/OrderLineCQueryUpdateServiceProvider.java
@@ -20,7 +20,7 @@ import com.sforce.soap.enterprise.sobject.SObject;
 public class OrderLineCQueryUpdateServiceProvider {
 	Logger logger = Logger.getLogger(OrderLineCQueryUpdateServiceProvider.class);
 	
-	public UpdateOutput orderLineCQureyOrUpdate(LineDetails lineDetails){
+	public UpdateOutput orderLineCQureyOrUpdate(LineDetails lineDetails) throws Exception{
 		logger.info("Entering - com.arris.sfdc.service.provider.OrderLineCQueryUpdateServiceProvider.orderLineCQureyOrUpdate(LineDetails) : "+lineDetails);
 		
 		UpdateOutput updateOutput = new UpdateOutput();
@@ -28,36 +28,39 @@ public class OrderLineCQueryUpdateServiceProvider {
 		
 		List<Inputs> listInputs = lineDetails.getInputs();
 		logger.info("listInputs Size : "+listInputs.size());
-				
-		EnterpriseConnection connection = SFDCConnection.getEnterpriseConnection();
-		if(connection != null){
-			String operation = listInputs.get(0).getOperation().trim();
-			logger.info("Operation = "+operation);
-			
+		
+		String operation = listInputs.get(0).getOperation().trim();
+		logger.info("Operation = "+operation);
+		
+		if(operation != null){
+					
 			//If Operation = Query 
-			if(OrderLineCOperationConstants.ORDER_LINE_C_OPERATION_QUERY.equalsIgnoreCase(operation)){
+			if(OrderLineCOperationConstants.ORDER_LINE_C_OPERATION_QUERY.equalsIgnoreCase(operation.trim())){
 				logger.info("Entering in Query Operation");
 				try{
-					Inputs inputs = listInputs.get(0);
-					String external_id__c = inputs.getExternalIDC();
-					logger.info("External_ID__c : "+external_id__c);
-					
-					if(external_id__c != null && external_id__c.length() > 0){
-						QueryResult queryResult = connection.query("select Id from Order_Line__c where External_ID__c = '"+external_id__c+"'");
-						if(queryResult != null){
-							logger.info("queryResult size : "+queryResult.getRecords().length);
-							
-							for(SObject sObject : queryResult.getRecords()){
-								Order_Line__c order_Line__c = (Order_Line__c) sObject;
+					EnterpriseConnection connection = SFDCConnection.getEnterpriseConnection();
+					if(connection != null){
+						Inputs inputs = listInputs.get(0);
+						String external_id__c = inputs.getExternalIDC();
+						logger.info("External_ID__c : "+external_id__c);
+						
+						if(external_id__c != null && external_id__c.length() > 0){
+							QueryResult queryResult = connection.query("select Id from Order_Line__c where External_ID__c = '"+external_id__c+"'");
+							if(queryResult != null){
+								logger.info("queryResult size : "+queryResult.getRecords().length);
 								
-								Results rs = new Results();
-								rs.setId(order_Line__c.getId());
-								rs.setErrors("");
-								rs.setSuccess("");
-								
-								logger.info("Results rs : "+rs);
-								
-								results.add(rs);
+								for(SObject sObject : queryResult.getRecords()){
+									Order_Line__c order_Line__c = (Order_Line__c) sObject;
+									
+									Results rs = new Results();
+									rs.setId(order_Line__c.getId());
+									rs.setErrors("");
+									rs.setSuccess("");
+									
+									logger.info("Results rs : "+rs);
+									
+									results.add(rs);
+								}
 							}
 						}
 					}
@@ -65,60 +68,76 @@ public class OrderLineCQueryUpdateServiceProvider {
 				}catch(Exception e){
 					logger.error("Error in Reading Object from Order_Line__c Object : "+e.getMessage());
 					e.printStackTrace();
+					
+					throw e;
 				}
 			}
 			
 			//If Operation = Update
-			if(OrderLineCOperationConstants.ORDER_LINE_C_OPERATION_UPDATE.equalsIgnoreCase(operation)){
+			if(OrderLineCOperationConstants.ORDER_LINE_C_OPERATION_UPDATE.equalsIgnoreCase(operation.trim())){
 				logger.info("Entering in Update Operation");
 				try{
-					Order_Line__c records[] = new Order_Line__c[listInputs.size()];
-					Iterator<Inputs> inputsIterator = listInputs.iterator();
-					int i = 0;
-					
-					while(inputsIterator.hasNext()){
-						Inputs inputs = inputsIterator.next();
-					
-						Order_Line__c order_Line__c = new Order_Line__c();
+					EnterpriseConnection connection = SFDCConnection.getEnterpriseConnection();
+					if(connection != null){
+						Order_Line__c records[] = new Order_Line__c[listInputs.size()];
+						Iterator<Inputs> inputsIterator = listInputs.iterator();
+						int i = 0;
 						
-						order_Line__c.setExternal_ID__c(inputs.getExternalIDC());
-						order_Line__c.setAvailable_Quantity__c(Double.parseDouble(inputs.getAvailableQuantityC()));
-						order_Line__c.setId(inputs.getId());
-						order_Line__c.setOnhand_Quantity__c(Double.parseDouble(inputs.getOnhandQuantityC()));
-												
-						records[i] = order_Line__c;
-						i++;
-					}
-					
-					logger.info("Order_Line__c Update Begins");
-					SaveResult saveResult[] = connection.update(records);
-					if(saveResult != null){
-						for(int j=0; j < saveResult.length; j++){
-							Results rs = new Results();
+						while(inputsIterator.hasNext()){
+							Inputs inputs = inputsIterator.next();
+						
+							Order_Line__c order_Line__c = new Order_Line__c();
 							
-							if(saveResult[j].getId() != null){
-								rs.setId(saveResult[j].getId());
-							}else{
-								rs.setId(listInputs.get(j).getId());
+							order_Line__c.setExternal_ID__c(inputs.getExternalIDC());
+							
+							String availableQuantityC = inputs.getAvailableQuantityC();
+							if(availableQuantityC != null && availableQuantityC.length() != 0){
+								order_Line__c.setAvailable_Quantity__c(Double.parseDouble(availableQuantityC.trim()));
 							}
 							
-							rs.setSuccess(String.valueOf(saveResult[j].getSuccess()));
+							order_Line__c.setId(inputs.getId());
 							
-							if(saveResult[j].getErrors().length > 0){
-								rs.setErrors(saveResult[j].getErrors()[0].getMessage());
+							String onHandQuantityC = inputs.getOnhandQuantityC();
+							if(onHandQuantityC != null && onHandQuantityC.length() != 0){
+								order_Line__c.setOnhand_Quantity__c(Double.parseDouble(onHandQuantityC.trim()));
 							}
-							else{
-								rs.setErrors(String.valueOf(false));
+													
+							records[i] = order_Line__c;
+							i++;
+						}
+						
+						logger.info("Order_Line__c Update Begins");
+						SaveResult saveResult[] = connection.update(records);
+						if(saveResult != null){
+							for(int j=0; j < saveResult.length; j++){
+								Results rs = new Results();
+								
+								if(saveResult[j].getId() != null){
+									rs.setId(saveResult[j].getId());
+								}else{
+									rs.setId(listInputs.get(j).getId());
+								}
+								
+								rs.setSuccess(String.valueOf(saveResult[j].getSuccess()));
+								
+								if(saveResult[j].getErrors().length > 0){
+									rs.setErrors(saveResult[j].getErrors()[0].getMessage());
+								}
+								else{
+									rs.setErrors(String.valueOf(false));
+								}
+								
+								results.add(rs);
+								
+								logger.info("Results rs : "+rs);
 							}
-							
-							results.add(rs);
-							
-							logger.info("Results rs : "+rs);
 						}
 					}
 				}catch(Exception e){
 					logger.error("Error in Updating Object in Order_Line__c Object : "+e.getMessage());
 					e.printStackTrace();
+					
+					throw e;
 				}
 			}
 		}
