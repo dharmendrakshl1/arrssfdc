e2018cb80186cded6f8f1742e86da44c1eb090e4
dharmendra.kshl1@gmail.com <dharmendra.kshl1@gmail.com>
2017-05-26 18:06:45 +0530

    Fixed error

--- sfdcorderstatusupsertheader/src/com/arris/sfdc/pojo/HeaderInput.java
+++ sfdcorderstatusupsertheader/src/com/arris/sfdc/pojo/HeaderInput.java
@@ -120,7 +120,7 @@ import javax.xml.bind.annotation.XmlType;
     "billToAddressC",
     "shipToAddressC",
     "psaProjectC",
-    "psaProjectC"
+    "corporateContractC"
 })
 @XmlRootElement(name = "HeaderInput")
 public class HeaderInput {
